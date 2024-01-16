3833cfcde0e012b675177669cf0595621d445027
MarcoR03113 <marcoroodt13@gmail.com>
2024-01-16 17:52:37 +0200

    2

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -19,7 +19,7 @@ const links = [
   },
   { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
-//dsdsadasd sdfhsodhfsodi asdasdas sadasda asdasd
+//dsdsadasd sdfhsodhfsodi asdasdas sadasda asdasd s
 export default function NavLinks() {
   const pathname = usePathname();
   return (
